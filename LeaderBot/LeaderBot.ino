56058adcf7bd8822f3ff1a7a6598039e83af68cf
David Wernli <Dave.Wernli@Gmail.com>
2014-07-17 23:37:52 +0000

    LeaderBot.ino edited online with Bitbucket

--- LeaderBot/LeaderBot.ino
+++ LeaderBot/LeaderBot.ino
@@ -3,19 +3,20 @@
 * ECE 450 - Semester Project Leader Robot
 * ***************************************************************************** */
 #include <Robot.h>
-#define LOG 0 
+#define LOG 0
 #define DB  1
 
 /*--- function declarations ---*/
 void serialEvent();
 void serialCommand(char ch);
+float PIDcalculate(float distance, int reset=0);
 
 /*--- sensor pins ---*/
-//const int irPinL = 14;    //pin reserved for Sharp IR input (analog)
-const int irPinR 	= 15;    //front IR sensor  
+const int irPinLR   = 14;    //pin reserved for Sharp IR input (analog)
+const int irPinLF   = 15;    //front IR sensor  
 const int pingPinR  = 52;  //pin reserved for ping sensor input (digital)
 const int pingPinF  = 53;
-const int cdsPin 	= 0;     //pin reserved for photoresistor input (analog)
+const int cdsPin  = 0;     //pin reserved for photoresistor input (analog)
 
 /*--- servo pins ---*/
 //const int servPin = 8   //pin reserved for servo output (PWM)
@@ -29,8 +30,8 @@ const int r_hPin1 = 3;  //right motor hbridge pins
 const int r_hPin2 = 2;
 
 /*--- global variables ---*/
-int speed = 80;       	//speed in PWM format [0 - 255]
-int mode  = -1;
+int speed = 70;       	//speed in PWM format [0 - 255]
+int mode  = 0;
 float setPoint;
 
 /*--- intitialize ---*/
@@ -52,35 +53,42 @@ void setup() {
 	//make sure the robot is stopped
 	robo.stop();
 	robo.setSpeed(speed); //set speed for turning/driving
-  robo.switchDirection(); //we'll be driving 'backwards', toward the little wheel.
-	if (DB) Serial.println(headers);
+  robo.swapWheels(); //we'll be driving 'backwards', toward the little wheel.
+  robo.swapDirectionPins();
 	delay(2000);	//give some time to put the robot down after reset
  }
 
 void loop(){
   static int status = 0;
   switch (mode) {
+    case -1:
+      robo.stop();
+      break;
     case 0: //calibrate & initialize
-      setPoint = 0;
+      setPoint = 50;
       //tell the others to get redy (turn on lights?)
       //have the Robot start
-      mode = 1;
+      mode += findCenter();
       break;
     case 1: //go straight
-      status = centerline();
+      status = centerLine();
       if (status != 0)
         mode = 20;
+      break;
     case 20: // corner approaching
       //log which way we are going to turn (toward the larger value)
       //if the values are more than 10% off, keep driving straight with the lower value
       //once they get close, switch mode to 21
+      break;
     case 21:  //time to turn
       //stop & pivot toward the turn direction until we pick up the next wall
       //hopefully we're still close to the center
+      break;
     case 22: //rotate until we find the next wall
       //rotate until we are centered again
       //once we're centered, drive straight again
-    case default:
+      break;
+    default:
       Serial.println("invalid mode selected");
   }
 }
@@ -90,13 +98,37 @@ void loop(){
  * esitmates hall width based on sensor readings
  * returns the width in meters
  */
-
+int findCenter(){
+  static int last_diff;
+  //get sensors
+  int front = robo.IRdistance_mm(irPinLF);
+  int rear  = robo.IRdistance_mm(irPinLR);
+  //if we are outside of the acceptible 'parallel' range
+  int difference = 100*abs(front - rear)/max(front, rear);  //percentage difference in the two readings
+  Serial.print("F "); Serial.print(front); 
+  Serial.print(" \tR "); Serial.print(rear);
+  Serial.print(" \tD "); Serial.println(difference);
+  if (difference > 2 ){
+    int dir = rear - front; //if positive we want to rotate CW
+    dir = max(-1, dir); //set floor
+    dir = min( 1, dir); //set ceiling
+    robo.pivot(dir, 90);      //pivot based on reading
+  } else {
+    Serial.println("AT ZERO");
+    if (last_diff == difference) {
+      return 1;
+    }   
+  }
+  last_diff = difference;
+  return 0;
+  //rotate toward shorter until they are within 5% of each other
+}
 int centerLine(){
-  const  int   numReadings
+  const  int   numReadings=5;
   const  int   MaxErr = 100; //the highest error value expected
-  static float readings_l[numReadings];
-  static float average_l = 0;
-  static float sum_l = 0;
+  static float readings_f[numReadings];
+  static float average_f = 0;
+  static float sum_f = 0;
   static float readings_r[numReadings];
   static float average_r = 0;
   static float sum_r = 0;
@@ -106,39 +138,46 @@ int centerLine(){
 
   //initialize the arrays
   if (first_call = 1) {
-    for (int i = 0; i < num; ++i)  {
-      readings_l[i] = 0;
+    for (int i = 0; i < numReadings; ++i)  {
+      readings_f[i] = 0;
       readings_r[i] = 0;
     }
     first_call = 0;
   }
   //update sum
-  sum_l -= readings_l[pointer];
-  sum_r -= readings_r[pointer];
+  //sum_f -= readings_f[pointer];
+  //sum_r -= readings_r[pointer];
   //get sensor readings
-  readings_l[pointer] = robo.IRdistance(irPinL);
-  readings_r[pointer] = robo.IRdistance(irPinR);
+  //readings_f[pointer] = robo.IRdistance(irPinLF);
+  //readings_r[pointer] = robo.IRdistance(irPinLR);
   //update sum
-  sum_l += readings_l[pointer];
-  sum_r += readings_r[pointer];
-
+  //sum_f += readings_f[pointer];
+  //sum_r += readings_r[pointer];
   //figure out if we found a corner
   //if the left reading jumped, we'll want to turn left
-  if (readings_l[pointer] > average_l*1.5) //50% greater than the average
-    return -1;
+  //if (readings_f[pointer] > average_f*1.5) //50% greater than the average
+    //return -1;
   //if the right reading jumped we'll want to turn right
-  if (readings_r[pointer] > average_r*1.5) 
-    return 1;
+  //if (readings_r[pointer] > average_r*1.5) 
+    //return 1;
   //update averages
-  average_l = sum_l/numReadings;
-  average_r = sum_r/numReadings;
+  average_f = robo.IRdistance(irPinLF); // = sum_f/numReadings;
+  average_r = robo.IRdistance(irPinLR); // = sum_f/numReadings;
   //turn according to the average
-  int error = average_l - average_r;
-  diff = PIDcalculate(error); //negative values mean too far right, turn left
+  if (average_r != 0 && average_f != 0)
+    float distance = ( average_f + average_r ) * 0.485;
+  else 
+    float distance = max(average_f, average_r) * 0.970;
+  float error = setPoint - distance;
+  float diff = PIDcalculate(error); //negative values mean too far right, turn left
   diff = min(diff, MaxErr); //limits error to max expected value
   diff = max(diff, -MaxErr);//limits error to min expected value
-  diff = map(error, -MaxErr, MaxErr, -20, 20);
-  robo.drive_dif(diff);
+  diff = map(error, -MaxErr, MaxErr, -50, 50);
+  robo.drive_dif(-(int)diff,speed);
+    //print stuff
+  Serial.print("DRIVING: F "); Serial.print(average_f);
+  Serial.print(" \tR "); Serial.print(average_r);
+  Serial.print(" \tD "); Serial.println(diff);
   return 0; //all systems normal
 }
 
@@ -185,13 +224,13 @@ void serialCommand(char ch){
   }
  }
 
-int PIDcalculate(distance){
+float PIDcalculate(float distance, int reset){
   //setup & tuning variables, 
   //below are for 300 RPM motors at 80/255 speed setting.
   const float Ku = 8.25;
   const float Tu = 3400;
   const float Kp = Ku*0.45;
-  const float Ki = Kp*2/Tu;
+  const float Ki = Kp*0.5/Tu;
   const float Kd = Kp*Tu/3;
   //terms used each time
   static float lastError = 0;  //the value of the last error - stored between function calls
@@ -204,10 +243,13 @@ int PIDcalculate(distance){
   float Pterm;                 //proportional term
   float Iterm;                 //integral term
   float Dterm;                 //derivative term
-  int output;
+  float output;
   //Use PID function from Lab #6
   error = (float)(setPoint - distance);
-  errorSum += (error*timeChange);       
+  if (reset==1)
+    errorSum = 0;
+  else
+    errorSum += (error*timeChange);       
   dError = (error - lastError)/timeChange;
   Pterm = (Kp*error);
   Iterm = (Ki*errorSum);
@@ -223,7 +265,7 @@ int PIDcalculate(distance){
     Serial.print(" \t"); Serial.print(Iterm);
     Serial.print(" \t"); Serial.print(Dterm);
     Serial.print(" \t"); Serial.print(output);
-    Serial.print(" \t"); Serial.print(now); 
+    Serial.print(" \t"); Serial.println(now); 
   }
   //
   return (output);
